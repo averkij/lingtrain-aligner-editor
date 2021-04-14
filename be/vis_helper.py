f81429032a370485cba2603e5958d4f02d398543
Sergei Averkiev <averoO@gmail.com>
2021-04-14 12:09:00 +0300

    Formatting.

--- be/vis_helper.py
+++ be/vis_helper.py
@@ -1,14 +1,19 @@
-from matplotlib import pyplot as plt
-import numpy as np
-import os
-import helper
+"""Visualization helper"""
+
 import json
+import os
+
+import helper
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def visualize_alignment_by_db(db_path, output_path, lang_name_from="ru", lang_name_to="de", batch_size=0):
+    """Visualize alignment using ids from the database"""
     if batch_size > 0:
         index = helper.get_clear_flatten_doc_index(db_path)
-        xs,ys = [],[]
-        for i,ix in enumerate(index):
+        xs, ys = [], []
+        for i, ix in enumerate(index):
             from_ids, to_ids = json.loads(ix[1]), json.loads(ix[3])
             for from_id in from_ids:
                 for to_id in to_ids:
@@ -18,37 +23,41 @@ def visualize_alignment_by_db(db_path, output_path, lang_name_from="ru", lang_na
         x_max, y_max = max(xs), max(ys)
         is_last = x_max % batch_size > 0
         total_batches = x_max//batch_size + 1 if is_last else 0
-        batches = [[[],[]] for _ in range(total_batches)]
+        batches = [[[], []] for _ in range(total_batches)]
 
-        for x,y in zip(xs, ys):
+        for x, y in zip(xs, ys):
             batch_id = x // batch_size
             batches[batch_id][0].append(x)
             batches[batch_id][1].append(y)
     else:
         index = helper.get_doc_index_original(db_path)
-        batches = [[[],[]] for _ in range(len(index))]
-        for i,batch in enumerate(index):
-            xs,ys = [],[]
+        batches = [[[], []] for _ in range(len(index))]
+        for i, batch in enumerate(index):
+            xs, ys = [], []
             for ix in batch:
                 from_ids, to_ids = json.loads(ix[1]), json.loads(ix[3])
                 for from_id in from_ids:
                     for to_id in to_ids:
                         xs.append(from_id)
                         ys.append(to_id)
-            for x,y in zip(xs, ys):
+            for x, y in zip(xs, ys):
                 batches[i][0].append(x)
                 batches[i][1].append(y)
 
-    for i,batch in enumerate(batches):
+    for i, batch in enumerate(batches):
         x_min, y_min = min(batch[0]), min(batch[1])
         x_max, y_max = max(batch[0]), max(batch[1])
         align_matrix = np.zeros((x_max-x_min, y_max-y_min))
-        for x,y in zip(batch[0], batch[1]):
-            align_matrix[x-x_min - 1,y-y_min - 1] = 1
-        save_pic(align_matrix, lang_name_to, lang_name_from, output_path, batch_number=i)
+        for x, y in zip(batch[0], batch[1]):
+            align_matrix[x-x_min - 1, y-y_min - 1] = 1
+        save_pic(align_matrix, lang_name_to, lang_name_from,
+                 output_path, batch_number=i)
+
 
 def save_pic(align_matrix, lang_name_to, lang_name_from, output_path, batch_number):
-    output = "{0}_{1}{2}".format(os.path.splitext(output_path)[0], batch_number, os.path.splitext(output_path)[1])
+    """Save the resulted picture"""
+    output = "{0}_{1}{2}".format(os.path.splitext(output_path)[
+                                 0], batch_number, os.path.splitext(output_path)[1])
     plt.figure(figsize=(3, 3))
 
     my_dpi = 100
