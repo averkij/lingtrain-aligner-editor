12737d87b74957e86f4feb0a164ffceaa5592c7c
Sergei Averkiev <averoO@gmail.com>
2021-04-23 13:04:39 +0300

    Update recalculate form.

--- fe/src/components/RecalculateBatchDialog.vue
+++ fe/src/components/RecalculateBatchDialog.vue
@@ -1,64 +1,96 @@
 <template>
   <v-dialog v-model="show" max-width="500px">
     <v-card>
-      <v-card-title>
-        Shift batch {{batch_id + 1}}
-      </v-card-title>
-      <!-- <v-card-text class="mt-5">
-        Window shift
-      </v-card-text> -->
-      <v-card-actions class="px-15 mt-10 d-flex justify-space-between">
-          <v-btn fab outlined color="gray" @click="shift-=stepSize">
-            <v-icon>mdi-arrow-left</v-icon>
-          </v-btn>
-          <div class="text-h4">
-            {{shift}}
-          </div>
-          <v-btn fab outlined color="gray" @click="shift+=stepSize">
-            <v-icon>mdi-arrow-right</v-icon>
-          </v-btn>
-      </v-card-actions>
-      <v-card-actions class="mt-10">
-        <v-btn color="primary" text @click="show=false">
-          Close
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" dark @click="recalculateBatch">
-          Recalculate
-        </v-btn>
-      </v-card-actions>
+      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+        <v-tab> Resolve </v-tab>
+        <v-tab> Recalculate </v-tab>
+      </v-tabs>
+
+      <div style="background: green; height: 250px">
+        <v-tabs-items v-model="tab" style="height: 100%">
+
+          <v-tab-item style="height: 100%">
+            <v-card class="d-flex flex-column" style="height: 100%">
+            <v-card-title class="justify-center pt-5 text-subtitle-1 px-10"
+              >Batch {{ batch_id + 1 }}. Resolve all found conflicts.</v-card-title
+            >
+            
+            <v-spacer></v-spacer>
+
+            <v-card-actions>
+              <v-btn color="primary" text @click="show = false"> Close </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="success" dark @click="recalculateBatch">
+                Resolve
+              </v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-tab-item>
+
+          <v-tab-item style="height: 100%">
+            <v-card class="d-flex flex-column" style="height: 100%">
+            <v-card-title class="justify-center pt-5 text-subtitle-1 px-10"
+              >Batch {{ batch_id + 1 }}. Select the shift for the second
+              text.</v-card-title
+            >
+            <v-card-actions class="px-15 mt-5 d-flex justify-space-between">
+              <v-btn fab outlined color="gray" @click="shift -= stepSize">
+                <v-icon>mdi-arrow-left</v-icon>
+              </v-btn>
+              <div class="text-h4">
+                {{ shift }}
+              </div>
+              <v-btn fab outlined color="gray" @click="shift += stepSize">
+                <v-icon>mdi-arrow-right</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-spacer></v-spacer>
+
+            <v-card-actions>
+              <v-btn color="primary" text @click="show = false"> Close </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" dark @click="recalculateBatch">
+                Recalculate
+              </v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </div>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-  export default {
-    name: "RecalculateBatchDialog",
-    props: {
-      value: Boolean,
-      batch_id: Number
+export default {
+  name: "RecalculateBatchDialog",
+  props: {
+    value: Boolean,
+    batch_id: Number,
+  },
+  data() {
+    return {
+      tab: null,
+      shift: 0,
+      stepSize: 20,
+    };
+  },
+  methods: {
+    recalculateBatch() {
+      this.show = false;
+      this.$emit("recalculateBatch", this.batch_id, this.shift);
     },
-    data() {
-      return {
-        shift: 0,
-        stepSize: 20
-      }
-    },
-    methods: {
-      recalculateBatch() {
-        this.show = false;
-        this.$emit('recalculateBatch', this.batch_id, this.shift)
+  },
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
       },
     },
-    computed: {
-      show: {
-        get() {
-          return this.value
-        },
-        set(value) {
-          this.$emit('input', value)
-        }
-      }
-    }
-  }
+  },
+};
 </script>
