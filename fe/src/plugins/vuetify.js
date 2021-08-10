c856227582e2e0da9e352d9a27846cece63d6686
Sergei Averkiev <averoO@gmail.com>
2021-08-10 22:15:17 +0300

    Init slider lib.

--- fe/src/plugins/vuetify.js
+++ fe/src/plugins/vuetify.js
@@ -1,10 +1,17 @@
 import "@mdi/font/css/materialdesignicons.css";
+import 'swiper/swiper-bundle.css'
+
+import SwiperClass, { Scrollbar } from "swiper";
+SwiperClass.use([Scrollbar]);
+
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import vueNumeralFilterInstaller from "vue-numeral-filter";
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 Vue.use(Vuetify);
 Vue.use(vueNumeralFilterInstaller, { locale: "en-gb" });
+Vue.use(VueAwesomeSwiper);
 
 export default new Vuetify({
   icons: {
